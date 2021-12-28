fe05075fc49d232dc90d8b5a4608ceef5eb00ddb
meowztk <vladimir.spodaneyko@instamart.ru>
2021-12-28 19:27:55 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,61 @@
-package hw02unpackstring
+package main
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+
+	"golang.org/x/example/stringutil"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(st string) (string, error) {
+	if len([]rune(st)) == 0 {
+		return st, nil
+	}
+	st = stringutil.Reverse(st)
+	digit_indexes := make(map[int]rune)
+	for i, char := range st {
+		if unicode.IsDigit(char) {
+			digit_indexes[i] = char
+		}
+	}
+	di_slice := make([]int, len(digit_indexes))
+	i := 0
+	for k := range digit_indexes {
+		di_slice[i] = k
+		i++
+	}
+	tmp := -1
+	for _, val := range di_slice {
+		if val == len(st)-1 {
+			return "", ErrInvalidString
+		}
+		if val-1 == tmp {
+			return "", ErrInvalidString
+		}
+		tmp = val
+	}
+
+	var b strings.Builder
+	multiplier := 1
+	for i, char := range st {
+		val, ok := digit_indexes[i]
+		if !ok {
+			b.WriteString(strings.Repeat(string(char), multiplier))
+			multiplier = 1
+		}
+		if ok {
+			multiplier, _ = strconv.Atoi(string(val))
+		}
+	}
+	return stringutil.Reverse(b.String()), nil
+}
+
+func main() {
+	st := "aaa0b"
+	fmt.Println(Unpack(st))
 }
